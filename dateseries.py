0ff8ee42a139c1ce1dc39d8a51cb03c4412099e4
amaralc <32170099+amaralc@users.noreply.github.com>
2018-01-13 02:28:34 -0200

    Update dateseries.py
    Add datetime method with conversion from str to dbl

--- dateseries.py
+++ dateseries.py
@@ -1,21 +1,20 @@
 
 
 
-from datetime import datetime
+import datetime
 
 def dateseries(yyyymmddhhmmss)
 
 	timestamp = str(yyyymmddhhmmss)
-	yyyy = timestamp[0:4]
-	mm = timestamp[4:6]
-	dd = timestamp[6:8]
-	hh = timestamp[8:10]
-	mm = timestamp[10:12]
-	ss = timestamp[12:14]
-	
-	# Use existing function from datetime module to convert strings in date
-	# i.e. 
-	# dateseries = datetime.datetime(yyyy,mm,dd,hh,mm,ss)
+	yyyy = dbl(timestamp[0:4])
+	mm = dbl(timestamp[4:6])
+	dd = dbl(timestamp[6:8])
+	hh = dbl(timestamp[8:10])
+	mm = dbl(timestamp[10:12])
+	ss = dbl(timestamp[12:14])
+
+  # Use existing function from datetime module to convert strings in date	
+	dateseries = datetime.datetime(yyyy,mm,dd,hh,mm,ss)
 	
 	return dataseries
 	
